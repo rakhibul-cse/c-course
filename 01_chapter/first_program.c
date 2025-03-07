d33a2f4a02a4192b5101136baf284525606420a8
rakhibul-cse <rakhibul.cse@gmail.com>
2025-03-07 21:27:42 +0600

    add program

--- 01_chapter/first_program.c
+++ 01_chapter/first_program.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World");
+    // add two numbers
+    int a, b;
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%d", &b);
+    int sum = a + b;
+    printf("Sum: %d", sum);
+
     return 0;
 }
